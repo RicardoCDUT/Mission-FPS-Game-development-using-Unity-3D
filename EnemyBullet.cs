22ccc818408ebe1a4323f87bc46c479d73b14882
Ricardo_M_L <94164453+RicardoCDUT@users.noreply.github.com>
2023-04-08 16:27:21 +0800

    EnemyBullet

---
+++ EnemyBullet.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    public float li;
+    float cd;
+    private Rigidbody rb;
+    //private AudioSource aunios;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //aunios.Play();
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = transform.TransformDirection(Vector3.forward * li);
+    }
+    private void Update()
+    {
+        cd += Time.deltaTime;
+        if (cd >= 3f)
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        print("子弹碰撞到了" + collision.gameObject.name);
+        Destroy(gameObject);
+       
+
+    }
+    //private void OnCollisionExit(Collision collision)
+    //{
+        
+    //}
+}
