b286479230ec7ae6ed51a9436ff6e3e3c9a0d70d
Ricardo_M_L <94164453+RicardoCDUT@users.noreply.github.com>
2023-04-08 16:32:21 +0800

    Destroy

---
+++ Drstroy_s.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drstroy_s : MonoBehaviour
+{
+    public float tims;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, tims);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
