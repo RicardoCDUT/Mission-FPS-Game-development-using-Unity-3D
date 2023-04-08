e62eb54a761c48829cb83cbd17ab4ec796d452bd
Ricardo_M_L <94164453+RicardoCDUT@users.noreply.github.com>
2023-04-08 16:28:11 +0800

    Look

---
+++ Look.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Look : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Camera.main!=null)
+        gameObject.transform.LookAt(Camera.main.transform);   //怪物的血条实时看向摄像机
+    }
+}
