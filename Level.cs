0ddb563930ed13e30902e43a29470dbe5190787e
Ricardo_M_L <94164453+RicardoCDUT@users.noreply.github.com>
2023-04-08 16:30:59 +0800

    Level

---
+++ Level.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour
+{
+    public static Level instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        Cursor.visible = true;  // 显示鼠标指针
+        Cursor.lockState = CursorLockMode.None; // 解除鼠标指针的锁定状态
+    }
+
+    // Update is called once per frame
+
+    public void Game0()
+    {
+        SceneManager.LoadScene(0);//跳转到场景1
+    }
+    public void Game1()
+    {
+        SceneManager.LoadScene(1);//跳转到场景1
+    }
+
+    public void Tx()
+    {
+        Application.Quit();
+    }
+    public void Qc()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+}
